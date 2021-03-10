a542d02f6437c39a3c033d37259c966e592f3c2c
corinacaterenciuc <corina.a.caterenciuc@gmail.com>
2021-03-10 16:38:13 +0200

    Initial commit

---
+++ src/test/java/ubb/CCA/AppTest.java
@@ -0,0 +1,25 @@
+package ubb.CCA;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void test1()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void test2()
+    {
+        assertTrue( true );
+    }
+}
