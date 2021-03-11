d35c5cc12e9e152c0152417c1693f9d8e9f8bea4
corinacaterenciuc <corina.a.caterenciuc@gmail.com>
2021-03-11 19:27:11 +0200

    first test

--- src/test/java/ubb/CCA/AppTest.java
+++ src/test/java/ubb/CCA/AppTest.java
@@ -3,6 +3,17 @@ package ubb.CCA;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import ubb.CCA.domain.Nota;
+import ubb.CCA.domain.Student;
+import ubb.CCA.domain.Tema;
+import ubb.CCA.repository.NotaXMLRepository;
+import ubb.CCA.repository.StudentXMLRepository;
+import ubb.CCA.repository.TemaXMLRepository;
+import ubb.CCA.service.Service;
+import ubb.CCA.validation.NotaValidator;
+import ubb.CCA.validation.StudentValidator;
+import ubb.CCA.validation.TemaValidator;
+import ubb.CCA.validation.Validator;
 
 /**
  * Unit test for simple App.
@@ -13,13 +24,25 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void test1()
+    public void tc_SuccessfulUserAdd()
     {
-        assertTrue( true );
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository studentRepo = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository temaRepo = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository notaRepo = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(studentRepo, temaRepo, notaRepo);
+
+        assertTrue( service.saveStudent("10", "TestStudent", 932) == 1 );
+        //assertTrue( true );
     }
     @Test
     public void test2()
     {
+
         assertTrue( true );
     }
 }
